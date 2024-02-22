9c1a7dda256828b7f0102f68d0df7d3d37d10cd8
Lotte Trijbels <a.j.t.p.trijbels@student.tudelft.nl>
2024-02-22 20:38:48 +0100

    I added comments to the expenses class

--- commons/src/main/java/commons/Expenses.java
+++ commons/src/main/java/commons/Expenses.java
@@ -12,6 +12,16 @@ public class Expenses {
     private List<Person> splittingOption;
     private String expenseType;
 
+    /**
+     * This is the constructor that initialises the expense
+     * @param person the person who paid
+     * @param category what the expense was for
+     * @param amount the amount that was spent
+     * @param currency what currency was used for the expense
+     * @param date when the expense was made
+     * @param splittingOption shows a list of people that are included in the splitting option
+     * @param expenseType the type of category the expense belongs to
+     */
     public Expenses(Person person, String category, int amount, String currency,
                     String date, List<Person> splittingOption, String expenseType) {
         this.person = person;
@@ -23,62 +33,123 @@ public class Expenses {
         this.expenseType = expenseType;
     }
 
+    /**
+     * Getter for person
+     * @return the person who paid for the expense
+     */
     public Person getPerson() {
         return person;
     }
 
+    /**
+     * Setter for person
+     * @param person the person who paid for the expense
+     */
     public void setPerson(Person person) {
         this.person = person;
     }
 
+    /**
+     * Getter for splittingOption
+     * @return the people the bill is split between
+     */
     public List<Person> getSplittingOption() {
         return splittingOption;
     }
 
+    /**
+     * Setter for splittingOption
+     * @param splittingOption the people the bill is split between
+     */
     public void setSplittingOption(List<Person> splittingOption) {
         this.splittingOption = splittingOption;
     }
 
+    /**
+     * Getter for category
+     * @return what the expense is for
+     */
     public String getCategory() {
         return category;
     }
 
+    /**
+     * Getter for category
+     * @param category what the expense is for
+     */
     public void setCategory(String category) {
         this.category = category;
     }
 
+    /**
+     * Getter for amount
+     * @return the price of the expense
+     */
     public int getAmount() {
         return amount;
     }
 
+    /**
+     * Setter for amount
+     * @param amount the price of the expense
+     */
     public void setAmount(int amount) {
         this.amount = amount;
     }
 
+    /**
+     * Getter for currency
+     * @return what currency was used for the expense
+     */
     public String getCurrency() {
         return currency;
     }
 
+    /**
+     * Setter for currency
+     * @param currency what currency was used for the expense
+     */
     public void setCurrency(String currency) {
         this.currency = currency;
     }
 
+    /**
+     * Getter for date
+     * @return when the expense was made
+     */
     public String getDate() {
         return date;
     }
 
+    /**
+     * Setter for date
+     * @param date when the expense was made
+     */
     public void setDate(String date) {
         this.date = date;
     }
 
+    /**
+     * Getter for expenseType
+     * @return what type of category the expense belongs to
+     */
     public String getExpenseType() {
         return expenseType;
     }
 
+    /**
+     * Setter for expenseType
+     * @param expenseType what type of category the expense belongs to
+     */
     public void setExpenseType(String expenseType) {
         this.expenseType = expenseType;
     }
 
+    /**
+     * Equals method for expenses
+     * @param o an object
+     * @return whether two expenses are equal or not
+     */
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
@@ -94,6 +165,10 @@ public class Expenses {
         return Objects.equals(expenseType, expenses.expenseType);
     }
 
+    /**
+     * Makes a hashcode
+     * @return the hashcode for the expense
+     */
     @Override
     public int hashCode() {
         int result = person != null ? person.hashCode() : 0;
@@ -106,6 +181,10 @@ public class Expenses {
         return result;
     }
 
+    /**
+     * Shows the details of the expense
+     * @return a string
+     */
     @Override
     public String toString() {
         StringBuilder res = new StringBuilder();
