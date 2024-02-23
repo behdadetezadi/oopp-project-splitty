72b9391c9ee6593d651ad47440509107b4c17d43
Behdad Etezadi <b.etezadi@student.tudelft.nl>
2024-02-23 21:37:25 +0100

    Added javadoc to the existing methods and attributes in the Event class

--- commons/src/main/java/commons/Event.java
+++ commons/src/main/java/commons/Event.java
@@ -17,21 +17,32 @@ public class Event {
     @GeneratedValue(strategy = GenerationType.AUTO)
     public long id;
 
+    //Event title
     private String title;
 
     //Todo: should be changed to a safer type for now we treat it as a number
+    //Event invite code
     private long inviteCode;
 
 
 
+    //List of Person s involved in the Event
     @OneToMany
     private List<Person> people;
 
+
+    //List of Expense s in the Event
     @OneToMany
     private List<Expense> expenses;
 
 
-
+    /**
+     * Constructor for Event
+     * @param people a List of Person s for the people involved in the Event
+     * @param expenses a List of Expense s made in the Event
+     * @param title a string stating the title of the Event
+     * @param inviteCode the invitation code for sharing the Event (for now type long)
+     */
     public Event(List<Person> people, List<Expense> expenses, String title, long inviteCode) {
         this.people = people;
         this.expenses = expenses;
@@ -39,19 +50,35 @@ public class Event {
         this.title = title;
     }
 
+    /**
+     * Empty public constructor (required)
+     */
     public Event() {
     }
 
+    /**
+     * Equals method using EqualsBuilder
+     * @param obj the object to check the equality with
+     * @return a boolean stating whether the object is equal to this or not (true if equal)
+     */
     @Override
     public boolean equals(Object obj) {
         return EqualsBuilder.reflectionEquals(this, obj);
     }
 
+    /**
+     * Hash method using HashCodeBuilder
+     * @return type int as a hashcode for this
+     */
     @Override
     public int hashCode() {
         return HashCodeBuilder.reflectionHashCode(this);
     }
 
+    /**
+     * toString method using ToStringBuilder using MULTI_LINE_STYLE (possibly needs to be changed in the future)
+     * @return a type String which is the presentation of this in a suitable String format
+     */
     @Override
     public String toString() {
         return ToStringBuilder.reflectionToString(this, MULTI_LINE_STYLE);
