2d09526b73e199b0e79cc2e8bee7930ff8047d02
Behdad Etezadi <b.etezadi@student.tudelft.nl>
2024-02-23 21:23:53 +0100

    made a basic Event class

--- commons/src/main/java/commons/Event.java
+++ commons/src/main/java/commons/Event.java
@@ -1,4 +1,59 @@
 package commons;
 
+import static org.apache.commons.lang3.builder.ToStringStyle.MULTI_LINE_STYLE;
+
+import jakarta.persistence.*;
+import org.apache.commons.lang3.builder.EqualsBuilder;
+import org.apache.commons.lang3.builder.HashCodeBuilder;
+import org.apache.commons.lang3.builder.ToStringBuilder;
+
+import java.util.List;
+
+@Entity
 public class Event {
+
+    //Event ID
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    public long id;
+
+    private String title;
+
+    //Todo: should be changed to a safer type for now we treat it as a number
+    private long inviteCode;
+
+
+
+    @OneToMany
+    private List<Person> people;
+
+    @OneToMany
+    private List<Expense> expenses;
+
+
+
+    public Event(List<Person> people, List<Expense> expenses, String title, long inviteCode) {
+        this.people = people;
+        this.expenses = expenses;
+        this.inviteCode = inviteCode;
+        this.title = title;
+    }
+
+    public Event() {
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        return EqualsBuilder.reflectionEquals(this, obj);
+    }
+
+    @Override
+    public int hashCode() {
+        return HashCodeBuilder.reflectionHashCode(this);
+    }
+
+    @Override
+    public String toString() {
+        return ToStringBuilder.reflectionToString(this, MULTI_LINE_STYLE);
+    }
 }
