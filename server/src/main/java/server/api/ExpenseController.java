0471b4614e84f64d7549cd3ad156257349b94995
Jodie Zhao <Y.Zhao-49@student.tudelft.nl>
2024-02-24 18:47:48 +0100

    ExpenseController and ExpenseControllerTest impelementation

--- server/src/main/java/server/api/ExpenseController.java
+++ server/src/main/java/server/api/ExpenseController.java
@@ -1,2 +1,95 @@
-package server.api;public class ExpenseController {
+package server.api;
+import commons.Expense;
+import commons.Person;
+import java.util.List;
+import java.util.stream.Collectors;
+
+
+
+public class ExpenseController {
+    private ExpenseModel expenseModel;
+
+    /**
+     * construct the ExpenseController object
+     * @param expenseModel the expenseModel object including a expenseList
+     */
+    public ExpenseController(ExpenseModel expenseModel)
+    {
+        this.expenseModel = expenseModel;
+    }
+    /**
+     * add an Expense object by passing individual parameters
+     * @param person the person who paid
+     * @param category what the expense was for
+     * @param amount the amount that was spent
+     * @param currency what currency was used for the expense
+     * @param date when the expense was made
+     * @param splittingOption shows a list of people that are included in the splitting option
+     * @param expenseType the type of category the expense belongs to
+     */
+
+    public void addExpense(Person person, String category,
+                           int amount, String currency, String date,
+                           List<Person> splittingOption, String expenseType)
+    {
+        Expense expense = new Expense(person, category, amount, currency, date, splittingOption, expenseType);
+        expenseModel.addExpense(expense);
+    }
+
+    /**
+     * show all Expense objects in the list of expenseModel object
+     * @return return expenseList of expenseModel object
+     */
+    public List<Expense> getAllExpenses() {
+        return expenseModel.getAllExpenses();
+    }
+    /**
+     * Display all expenses specific to that date.
+     * @param date all the expenses of certain date that the user want to check
+     */
+    public List<Expense> filterExpensesByDate(String date) {
+        return expenseModel.getAllExpenses().stream()
+                .filter(expense -> expense.getDate().equals(date))
+                .collect(Collectors.toList());
+    }
+    /** add money transfer form person A to B(I'm not sure if the requirement is need to add the transfer in to the expenseList in the expenseModel)
+     * @param A the person who need to transfer the money
+     * @param B the person who receive the transfer
+     * @param amount the amount of transfer
+     * @param currency the currency of the transfer
+     * @param date the date of the transfer
+     */
+    public void addMoneyTransfer(Person A, Person B, int amount, String currency, String date)
+    {
+        Expense transfer = new Expense(A, "Money Transfer", amount, currency, date, List.of(B), "Transfer");
+        expenseModel.getAllExpenses().add(transfer);
+    }
+    /**
+     * Display all expenses specific to the person.
+     * @param person the expense of certain person that the user want to check
+     */
+    public List<Expense> filterExpensesByPerson(Person person)
+    {
+        return expenseModel.getAllExpenses().stream()
+                .filter(expense -> expense.getPerson().equals(person))
+                .collect(Collectors.toList());
+    }
+    /**
+     * Display all expenses involving certain person.
+     * @param person the expense of certain person that the user want to check
+     */
+    public List<Expense> filterExpensesInvolvingSomeone(Person person) {
+        return expenseModel.getAllExpenses().stream()
+                .filter(expense -> expense.getSplittingOption().contains(person))
+                .collect(Collectors.toList());
+    }
+    /**
+     * Display all the details of certain expense.
+     * @param expense the expense that the user want to check details
+     */
+    public String getExpenseDetails(Expense expense)
+    {
+        return expense.toString();
+    }
+
 }
--- server/src/test/java/server/api/ExpenseControllerTest.java
+++ server/src/test/java/server/api/ExpenseControllerTest.java
@@ -1,2 +1,113 @@
-package server.api;public class ExpenseControllerTest {
+package server.api;
+
+import commons.Expense;
+import commons.Person;
+import org.junit.jupiter.api.*;
+import java.util.List;
+import static org.junit.jupiter.api.Assertions.*;
+
+class ExpenseControllerTest
+{
+
+    private ExpenseController expenseController;
+    private ExpenseModel expenseModel;
+
+    @BeforeEach
+    void start()
+    {
+        expenseModel = new ExpenseModel();
+        expenseController = new ExpenseController(expenseModel);
+    }
+
+    @Test
+    void addExpense()
+    {
+        Person person = new Person("Jodie","Zhao");
+        String category = " CSE tution fee";
+        int amount = 16000;
+        String currency = "euro";
+        String date = "2023-08-27";
+        List<Person> splittingOption = List.of(person);
+        String expenseType = "Education";
+        expenseController.addExpense(person, category, amount, currency, date, splittingOption, expenseType);
+
+        List<Expense> expenses = expenseController.getAllExpenses();
+        assertEquals(1, expenses.size());
+        assertEquals(person, expenses.get(0).getPerson());
+        assertEquals(category, expenses.get(0).getCategory());
+        assertEquals(amount, expenses.get(0).getAmount());
+        assertEquals(currency, expenses.get(0).getCurrency());
+        assertEquals(date, expenses.get(0).getDate());
+        assertEquals(splittingOption, expenses.get(0).getSplittingOption());
+        assertEquals(expenseType, expenses.get(0).getExpenseType());
+    }
+
+    @Test
+    void filterExpensesByDate()
+    {
+        addExpense();
+        String date = "2023-08-27";
+        List<Expense> filteredExpenses = expenseController.filterExpensesByDate(date);
+        assertEquals(1, filteredExpenses.size());
+        assertEquals(date, filteredExpenses.get(0).getDate());
+    }
+
+    @Test
+    void addMoneyTransfer() {
+        Person personA = new Person("Yanran","Zhao");
+        Person personB = new Person("Jodie","Zhao");
+        int amount = 100;
+        String currency = "euro";
+        String date = "2024-02-24";
+
+        expenseController.addMoneyTransfer(personA, personB, amount, currency, date);
+
+        List<Expense> expenses = expenseController.getAllExpenses();
+        assertEquals(1, expenses.size());
+        assertEquals("Money Transfer", expenses.get(0).getCategory());
+        assertEquals(amount, expenses.get(0).getAmount());
+        assertEquals(currency, expenses.get(0).getCurrency());
+        assertEquals(date, expenses.get(0).getDate());
+        assertTrue(expenses.get(0).getSplittingOption().contains(personB));
+        assertEquals("Transfer", expenses.get(0).getExpenseType());
+    }
+
+    @Test
+    void filterExpensesByPerson()
+    {
+        addExpense();
+        Person person= new Person("Jodie","Zhao");
+        List<Expense> filteredExpenses = expenseController.filterExpensesByPerson(person);
+        assertEquals(1, filteredExpenses.size());
+        assertEquals(person, filteredExpenses.get(0).getPerson());
+    }
+
+    @Test
+    void filterExpensesInvolvingSomeone()
+    {
+        addExpense();
+
+        Person person = new Person("Jodie","Zhao");
+        List<Expense> filteredExpenses = expenseController.filterExpensesInvolvingSomeone(person);
+
+        assertEquals(1, filteredExpenses.size());
+        assertTrue(filteredExpenses.get(0).getSplittingOption().contains(person));
+    }
+
+    @Test
+    void getExpenseDetails()
+    {
+        addExpense();
+        Expense expense = expenseController.getAllExpenses().get(0);
+        String details = expenseController.getExpenseDetails(expense);
+
+        if(details != null)
+        {
+            assertTrue(details.contains("Jodie"));
+            assertTrue(details.contains("CSE tution fee"));
+            assertTrue(details.contains("16000"));
+            assertTrue(details.contains("euro"));
+            assertTrue(details.contains("2023-08-27"));
+        }
+    }
 }
