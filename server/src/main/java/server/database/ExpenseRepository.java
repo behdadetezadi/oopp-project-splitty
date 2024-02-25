aa8826fa231d73b2822ebfc9c11ca67a0f3eae67
Behdad Etezadi <b.etezadi@student.tudelft.nl>
2024-02-25 16:24:59 +0100

    made empty repository innterfaces

---
+++ server/src/main/java/server/database/ExpenseRepository.java
@@ -0,0 +1,9 @@
+package server.database;
+
+
+import commons.Expense;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+
+
+public interface ExpenseRepository extends JpaRepository<Expense, Long> {}
