647e94cc2f2ab338d7283bd207f3809cad02f2e9
Vuk Pejić <v.pejic@student.tudelft.nl>
2024-03-08 22:07:16 +0100

    Increased attributes to be displayed by using the larger participant constructor

--- client/src/main/java/client/scenes/TableOfParticipantsController.java
+++ client/src/main/java/client/scenes/TableOfParticipantsController.java
@@ -11,6 +11,9 @@ import javafx.scene.layout.VBox;
 import javafx.collections.ObservableList;
 import javafx.collections.FXCollections;
 
+import java.util.HashMap;
+import java.util.HashSet;
+
 public class TableOfParticipantsController {
 
     @FXML
@@ -32,6 +35,11 @@ public class TableOfParticipantsController {
             StringBuilder sb = new StringBuilder();
             sb.append("First Name: "+ participant.getFirstName()+'\n');
             sb.append("Last Name: "+ participant.getLastName()+'\n');
+            sb.append("Username: "+ participant.getUsername()+'\n');
+            sb.append("Email: "+ participant.getEmail()+'\n');
+            sb.append("IBAN: "+ participant.getIban()+'\n');
+            sb.append("BIC: "+ participant.getBic()+'\n');
+            sb.append("Language Preference: "+ participant.getLanguageChoice()+'\n');
             String content = sb.toString();
             Label label = new Label(content);
             label.getStyleClass().add("participant-label");
@@ -43,9 +51,12 @@ public class TableOfParticipantsController {
 
     private void loadParticipants() {
         participants.addAll(
-                new Participant("John", "Doe"),
-                new Participant("Jane", "Doe"),
-                new Participant("Joshy","Doe")
+                new Participant("johnDoe", "John", "Doe","john.doe@student.tudelft.com","A1","B2",
+                        new HashMap<>(), new HashMap<>(), new HashSet<>(),"English"),
+                new Participant("janeDoe", "Jane", "Doe","jane.doe@student.tudelft.com","A1","B2",
+                        new HashMap<>(), new HashMap<>(), new HashSet<>(),"Dutch"),
+                new Participant("joshDoe", "Josh", "Doe","josh.doe@student.tudelft.com","A1","B2",
+                        new HashMap<>(), new HashMap<>(), new HashSet<>(),"English")
         );
     }
 }
