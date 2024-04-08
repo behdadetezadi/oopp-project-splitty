e8d7df1eff06a1262bd49143a51f3a6c471516a5
Maks Kuźnia <M.J.Kuznia@student.tudelft.nl>
2024-04-08 15:27:01 +0200

    Fix duplicate events in AdminPage

--- client/src/main/java/client/scenes/MainController.java
+++ client/src/main/java/client/scenes/MainController.java
@@ -156,7 +156,6 @@ public class MainController {
     public void showAdminPage() {
         primaryStage.setTitle("Admin Page");
         primaryStage.setScene(adminScene);
-        adminController.initialize();
     }
     /**
      * shows the StartPage
