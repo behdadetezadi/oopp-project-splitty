3609e6a1fad411bf1c1189af89a7c53566b6fa10
Lotte Trijbels <a.j.t.p.trijbels@student.tudelft.nl>
2024-04-09 21:02:58 +0200

    fixed statistics more

--- client/src/main/java/client/scenes/StatsCtrl.java
+++ client/src/main/java/client/scenes/StatsCtrl.java
@@ -6,20 +6,11 @@ import commons.Event;
 import commons.Expense;
 import javafx.beans.binding.Bindings;
 import javafx.fxml.FXML;
-import javafx.geometry.Pos;
 import javafx.scene.Node;
 import javafx.scene.chart.PieChart;
-
-import javafx.scene.control.Alert;
 import javafx.scene.control.Label;
-import javafx.fxml.Initializable;
-import javafx.scene.control.ProgressIndicator;
 import javafx.scene.control.Tooltip;
 import javafx.scene.input.MouseEvent;
-import javafx.scene.layout.HBox;
-import javafx.scene.layout.VBox;
-import javafx.scene.paint.Color;
-import javafx.scene.shape.Circle;
 import javafx.stage.Stage;
 
 import java.text.DecimalFormat;
@@ -31,10 +22,6 @@ public class StatsCtrl {
     private PieChart pieChart;
     @FXML
     private Label cost;
-    @FXML
-    private ProgressIndicator progressIndicator;
-    @FXML
-    private VBox chartLegend;
     private Locale activeLocale;
     private ResourceBundle resourceBundle;
     private MainController mainController;
@@ -54,9 +41,8 @@ public class StatsCtrl {
     }
 
     public void initialize() {
-//        pieChart.setVisible(false);
-//        this.resourceBundle = resourceBundle;
         fillPieChart(event.getId());
+        addEventHandlersToSlices();
     }
 
     private void fillPieChart(Long eventId) {
@@ -67,32 +53,17 @@ public class StatsCtrl {
         tagAndExpense.forEach((tag, amount) -> {
             PieChart.Data slice = new PieChart.Data(tag, amount);
             pieChart.getData().add(slice);
-//            colorSlice(slice, "color");
         });
         totalCost = (tagAndExpense.values().stream().mapToDouble(Double::doubleValue).sum());
-        cost.setText("$"+totalCost);
-
+        cost.setText("Total cost: $"+numberFormat.format(totalCost));
 
         pieChart.getData().forEach(data ->
                 data.nameProperty().bind(
                         Bindings.concat(
                                 data.getName(), ": ", data.pieValueProperty().getValue(),
-                                " (", data.pieValueProperty().getValue() * 100 / totalCost, "%)")));
-        pieChart.setLabelsVisible(false);
-        pieChart.setLegendVisible(false);
-        createCustomLegend(tagAndExpense);
+                                " (", numberFormat.format(data.pieValueProperty().getValue() * 100 / totalCost), "%)")));
 
-        pieChart.setVisible(true);
     }
-
-    private String translateTag(String tag) {
-        String displayName = tag;
-        if ("food".equals(tag) || "travel".equals(tag) || "entrance_fees".equals(tag)) {
-            displayName = resourceBundle.getString(tag);
-        }
-        return displayName;
-    }
-
 
     private HashMap<String, Double> tagExpense(Set<Expense> expenses) {
         HashMap<String, Double> tagAndExpense = new HashMap<>();
@@ -105,12 +76,6 @@ public class StatsCtrl {
             }
         }
         return tagAndExpense;
-    }
-
-
-    private void colorSlice(PieChart.Data data, String color) {
-        Node slice = data.getNode();
-        slice.setStyle("-fx-pie-color: " + color.toLowerCase() + ";");
     }
 
     /**
@@ -147,25 +112,6 @@ public class StatsCtrl {
             slice.getNode().setUserData(slice);
         });
     }
-
-    private void createCustomLegend(HashMap<String, Double> tagAndExpense) {
-        chartLegend.getChildren().clear();
-        tagAndExpense.forEach((tag, amount) -> {
-            HBox legendItem = new HBox(10);
-            legendItem.setAlignment(Pos.CENTER_LEFT);
-            Circle colorCircle = new Circle(8);
-            colorCircle.setFill(Color.web("gray"));
-            Label label = null;
-            label = new Label(tag + ": " +
-                    numberFormat.format(amount) +
-                    " (" + numberFormat.format(amount * 100 / totalCost) + "%)");
-
-            legendItem.getChildren().addAll(colorCircle, label);
-            chartLegend.getChildren().add(legendItem);
-        });
-
-    }
-
     @FXML
     private void switchToExpenseOverviewScene() {
         mainController.showExpenseOverview(event, activeLocale);
