9f1c43721fe3959d86931d9c1ac5c30ff761b35f
Tijn Friederich <t.w.friederich@student.tudelft.nl>
2024-04-13 18:50:36 +0200

    added a test for expense ParticipantOption

---
+++ client/src/test/java/client/scenes/ParticipantOptionTest.java
@@ -0,0 +1,18 @@
+package client.scenes;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class ParticipantOptionTest {
+
+    @Test
+    public void participantOptionTest() {
+        ParticipantOption p = new ParticipantOption((long)21, "Eliot");
+        assertEquals(21, p.getId());
+        assertEquals("Eliot", p.toString());
+    }
+
+
+
+}
